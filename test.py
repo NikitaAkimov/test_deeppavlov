ec193077640c80b41026f42013650dcdc6a24f0e
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-02 01:55:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from deeppavlov import configs
 from deeppavlov.core.commands.infer import build_model
-odqa = build_model(configs.odqa.en_odqa_infer_wiki, load_trained=True)
+odqa = build_model(configs.odqa.en_odqa_infer_wiki)
 answers = odqa(["Where did guinea pigs originate?", "When did the Lynmouth floods happen?", "When is the Bastille Day?"])
