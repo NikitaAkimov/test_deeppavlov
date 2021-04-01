bf74d9df9750d1fc4dd72a75fbe7e7502d7c2c32
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-02 00:41:45 +0300

    Create obuchenie.py

---
+++ obuchenie.py
@@ -0,0 +1,17 @@
+from deeppavlov import configs
+from deeppavlov.core.commands.infer import build_model
+from deeppavlov.core.common.file import read_json
+from deeppavlov import configs, train_model
+
+model_config = read_json(configs.doc_retrieval.ru_ranker_tfidf_wiki)
+model_config["dataset_reader"]["data_path"] = "/root/deepSearch_DoctorAi_new/test_deeppavlov/data_deases.csv"
+model_config["dataset_reader"]["dataset_format"] = "txt"
+model_config["train"]["batch_size"] = 1000
+
+doc_retrieval = train_model(model_config)
+
+# Download all the SQuAD models
+squad = build_model(configs.squad.multi_squad_noans_infer, download = True)
+# Do not download the ODQA models, we've just trained it
+odqa = build_model(configs.odqa.ru_odqa_infer_wiki, download = False)
+odqa(["Что такое бронхит?"])
