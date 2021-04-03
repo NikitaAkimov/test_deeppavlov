80e78561fc18c1595c44aa25add6bd7fd1fb99ae
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-04 02:50:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from deeppavlov import configs
 from deeppavlov.core.commands.infer import build_model
 odqa = build_model(configs.odqa.en_odqa_infer_wiki, download = False)
-result = odqa(['What is the name of Darth Vader\'s son?'])
+result = odqa(['Who is the Greek god of War?'])
 print(result)
