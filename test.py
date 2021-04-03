9d3321f87bc21014652ed51343b5f0727dfc8427
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-04 02:03:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from deeppavlov import configs
 from deeppavlov.core.commands.infer import build_model
-odqa = build_model(configs.odqa.en_odqa_infer_wiki, download = False)
-answers = odqa(["Where did guinea pigs originate?", "When did the Lynmouth floods happen?", "When is the Bastille Day?"])
+odqa = build_model(configs.odqa.ru_odqa_infer_wiki, download = True)
+answers = odqa(["Сколько детей родилось в 2008 году у граждан Швейцарии?"])
 print(answers)
