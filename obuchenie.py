71a13fac28d7bd1478c84e74f734ec6ed3855648
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-04 15:49:01 +0300

    Update obuchenie.py

--- obuchenie.py
+++ obuchenie.py
@@ -6,7 +6,7 @@ from deeppavlov import configs, train_model
 model_config = read_json(configs.doc_retrieval.ru_ranker_tfidf_wiki)
 model_config["dataset_reader"]["data_path"] = "/root/deepSearch_DoctorAi_new/test_deeppavlov/model.csv"
 model_config["dataset_reader"]["dataset_format"] = "txt"
-model_config["train"]["batch_size"] = 1000
+#model_config["train"]["batch_size"] = 1000
 
 doc_retrieval = train_model(model_config)
 doc_retrieval(['Бронхит'])
