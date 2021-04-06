1c50a87bd7def6ff2c2a3bf7b6481860acdae99f
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-06 19:33:54 +0300

    Update obuchenie.py

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,6 @@
-from deeppavlov.deprecated.skills.similarity_matching_skill import SimilarityMatchingSkill
+from deeppavlov import configs
+from deeppavlov.core.commands.infer import build_model
 
-faq_skill = SimilarityMatchingSkill(data_path = './model.csv',
-                              x_col_name = 'Question', 
-                              y_col_name = 'Answer',
-                              save_load_path = './model',
-                              config_type = 'tfidf_autofaq',
-                              train = True)
-a = faq_skill(['Что такое бронхит?'], [], [])
+faq = build_model(configs.faq.tfidf_autofaq, download = True)
+a = faq(["мне нужен код регистрации"])
 print(a)
