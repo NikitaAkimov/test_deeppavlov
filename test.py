232f892fb510b9bfcf642a5b7e4840c71a16d329
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-21 22:19:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from deeppavlov.deprecated.skills.similarity_matching_skill import SimilarityMatchingSkill
+from deeppavlov.agents.default_agent.default_agent import DefaultAgent
+from deeppavlov.agents.processors.highest_confidence_selector import HighestConfidenceSelector
+from deeppavlov.utils.server import start_agent_server
 
 faq_skill = SimilarityMatchingSkill(data_path = '/root/test_prog/test_deeppavlov/models1.csv',
                               x_col_name = 'Question', 
@@ -8,3 +11,7 @@ faq_skill = SimilarityMatchingSkill(data_path = '/root/test_prog/test_deeppavlov
                               train = True)
 
 print(faq_skill(['Каккие симптомы пневмонии?'], [], []))
+
+agent = DefaultAgent([faq_skill], skills_selector=HighestConfidenceSelector())
+
+start_model_server(agent, port=5000)
