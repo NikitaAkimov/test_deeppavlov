d1fe5843127d8c934f5ddfaef4b5babfec6807af
NikitaAkimov <33234557+NikitaAkimov@users.noreply.github.com>
2021-04-21 22:51:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
-from deeppavlov.deprecated.skills.similarity_matching_skill import SimilarityMatchingSkill
-from deeppavlov.deprecated.agents.default_agent.default_agent import DefaultAgent
-from deeppavlov.deprecated.agents.processors.highest_confidence_selector import HighestConfidenceSelector
-from deeppavlov.utils.server import start_model_server
+# from deeppavlov.deprecated.skills.similarity_matching_skill import SimilarityMatchingSkill
+# from deeppavlov.deprecated.agents.default_agent.default_agent import DefaultAgent
+# from deeppavlov.deprecated.agents.processors.highest_confidence_selector import HighestConfidenceSelector
+# from deeppavlov.utils.server import start_model_server
 
-faq_skill = SimilarityMatchingSkill(data_path = '/root/test_prog/test_deeppavlov/models1.csv',
-                              x_col_name = 'Question', 
-                              y_col_name = 'Answer',
-                              save_load_path = '/root/test_prog/model',
-                              config_type = 'tfidf_autofaq',
-                              train = True)
+# faq_skill = SimilarityMatchingSkill(data_path = '/root/test_prog/test_deeppavlov/models1.csv',
+#                               x_col_name = 'Question', 
+#                               y_col_name = 'Answer',
+#                               save_load_path = '/root/test_prog/model',
+#                               config_type = 'tfidf_autofaq',
+#                               train = True)
 
-print(faq_skill(['Каккие симптомы пневмонии?'], [], []))
+# print(faq_skill(['Каккие симптомы пневмонии?'], [], []))
 
-agent = DefaultAgent([faq_skill], skills_selector=HighestConfidenceSelector())
+# agent = DefaultAgent([faq_skill], skills_selector=HighestConfidenceSelector())
 
-start_model_server(model_config=agent)
+# start_model_server(model_config=agent)
+
+from deeppavlov import configs, train_model
+
+model_config = read_json(configs.faq.tfidf_logreg_en_faq)
+model_config["dataset_reader"]["data_path"] = "/content/faq.csv"
+model_config["dataset_reader"]["data_url"] = None
+faq = train_model(model_config)
+a = faq(["tell me about DeepPavlov"])
+a
